ba38d5710a08e30eaa29dcb44265cbb546fb7565
styujk2 <chatgithup@gmail.com>
2025-01-11 11:43:36 +0300

    Add files via upload

---
+++ From/from/bbb.js
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<HTML lang="ar">
+<head>
+    <meta charset="UTF_8"/>
+    <link rel="stylesheet" href="style.css">
+    <script src="jquery-3.7.1.min.js"></script>
+    <title>Properties</title>
+</head>
+<body>
+
+<center>
+    <button id="complate">متابعة</button>
+    <br/>
+    <br/>
+    
+    <form id="info">
+        <input type="text" id="fulname" placeholder="الاسم" />
+        <br />
+        <br />
+        <input type="text" id="NumId" placeholder="الرقم الوطني" required pattern="[11]" />
+        <br />
+        <br />
+        <input type="date" placeholder="التاريخ " />
+        <br />
+        <br />
+        <input type="tel" id="number" placeholder="رقم الهاتف" pattern="09[0-9]{8}"  />
+        <br />
+        <br />
+        <input type="email" placeholder="الايميل" />
+        <br />
+        <br />
+        <button id="end">ارسال</button>
+    </form>
+                   
+    
+    
+    </center>
+    
+      <script src="jquery-3.7.1.min.js"></script>
+      <script src="myFunctions.js"></script>
+    </body>
+    </HTML>
